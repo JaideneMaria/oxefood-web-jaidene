4ac77c16c5a19a25453479ae300d8924df7ca532
Jaidene Maria <jaidene_silva@hotmail.com>
2024-11-04 11:28:51 -0300

    Finalização das telas

--- src/views/entregador/FormEntregador.jsx
+++ src/views/entregador/FormEntregador.jsx
@@ -1,199 +1,171 @@
 import React from "react";
 import InputMask from 'react-input-mask';
-import { Button, Container, Divider, Form, Icon, Select, Radio } from 'semantic-ui-react';
+import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
 
 export default function FormProduto() {
-
     const countryOptions = [
         { key: 'pe', value: 'pe', text: 'Pernambuco' },
         { key: 'al', value: 'al', text: 'Alagoas' },
         { key: 'ce', value: 'ce', text: 'Ceará' },
+    ];
 
-    ]
     return (
 
         <div>
+            <Container textAlign='justified' style={{ marginTop: '3%' }}>
+                <h2>
+                    <span style={{ color: 'darkgray' }}>
+                        Entregador &nbsp;<Icon name='angle double right' size="small" />
+                    </span>
+                    Cadastro
+                </h2>
+                <Divider />
 
-            <div style={{ marginTop: '3%' }}>
+                <Form style={{ marginTop: '4%' }}>
+                    <Form.Group widths='equal'>
+                        <Form.Input
+                            required
+                            fluid
+                            label='Nome'
+                            maxLength="100"
+                        />
+                        <Form.Input
+                            required
+                            fluid
+                            label='CPF'
+                        >
+                            <InputMask
+                                required
+                                mask="999.999.999-99"
+                            />
+                        </Form.Input>
+                        <Form.Input
+                            fluid
+                            label='RG'
+                            maxLength="20"
+                        />
+                    </Form.Group>
 
-                <Container textAlign='justified' >
+                    <Form.Group widths='equal'>
+                        <Form.Input
+                            fluid
+                            label='Data de Nascimento'
+                        >
+                            <InputMask
+                                mask="99/99/9999"
+                                maskChar={null}
+                                placeholder="Ex: 20/03/1985"
+                            />
+                        </Form.Input>
+                        <Form.Input
+                            required
+                            fluid
+                            label='Fone Celular'
+                        >
+                            <InputMask
+                                mask="(99) 9999.9999"
+                            />
+                        </Form.Input>
+                        <Form.Input
+                            fluid
+                            label='Fone Fixo'
+                        >
+                            <InputMask
+                                mask="(99) 9999.9999"
+                            />
+                        </Form.Input>
+                        <Form.Input
+                            fluid
+                            label='QTD de Entregas realizadas'
+                        />
+                        <Form.Input
+                            fluid
+                            label='Valor por Frete'
+                        />
+                    </Form.Group>
 
-                    <h2> <span style={{ color: 'darkgray' }}> Entregador &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+                    <Form.Group widths='equal'>
+                        <Form.Input
+                            fluid
+                            label='Rua'
+                        />
+                        <Form.Input
+                            fluid
+                            label='Número'
+                            width={4}
+                        />
+                    </Form.Group>
 
-                    <Divider />
+                    <Form.Group widths='equal'>
+                        <Form.Input
+                            fluid
+                            label='Bairro'
+                        />
+                        <Form.Input
+                            fluid
+                            label='Cidade'
+                        />
+                        <Form.Input
+                            fluid
+                            label='CEP'
+                        />
+                    </Form.Group>
+
+                    <Form.Field>
+                        <label>UF</label>
+                        <Form.Select
+                            placeholder='Selecione'
+                            options={countryOptions}
+                        />
+                    </Form.Field>
+
+                    <Form.Input
+                        fluid
+                        label='Complemento'
+                    />
+
+                    <Form.Field>
+                        <label>Ativo</label>
+                        <Form.Radio
+                            label='Sim'
+                            name="radioGroup"
+                            value="sim"
+                            style={{ marginRight: '1em' }}
+                        />
+                        <Form.Radio
+                            label='Não'
+                            name="radioGroup"
+                            value="não"
+                        />
+                    </Form.Field>
 
                     <div style={{ marginTop: '4%' }}>
-
-                        <Form>
-
-                            <Form.Group widths='equal'>
-
-                                <Form.Input
-                                    required
-                                    fluid
-                                    label='Nome'
-                                    maxLength="100"
-                                />
-
-                                <Form.Input
-                                    required
-                                    fluid
-                                    label='CPF'>
-                                    <InputMask
-                                        required
-                                        mask="999.999.999-99"
-                                    />
-                                </Form.Input>
-
-                                <Form.Input
-                                    fluid
-                                    label='RG'
-                                    maxLength="20"
-                                />
-
-                            </Form.Group>
-
-                            <Form.Group widths='equal'>
-
-                                <Form.Input
-                                    fluid
-                                    label='Data de Nascimento'
-                                    width={6}
-                                >
-                                    <InputMask
-                                        mask="99/99/9999"
-                                        maskChar={null}
-                                        placeholder="Ex: 20/03/1985"
-                                    />
-
-                                </Form.Input>
-
-
-                                <Form.Input
-                                    required
-                                    fluid
-                                    label='Fone Celular'
-                                    width={6}>
-                                    <InputMask
-                                        mask="(99) 9999.9999"
-                                    />
-                                </Form.Input>
-
-                                <Form.Input
-                                    fluid
-                                    label='Fone Fixo'
-                                    width={6}>
-                                    <InputMask
-                                        mask="(99) 9999.9999"
-                                    />
-                                </Form.Input>
-
-                                <Form.Input
-                                    fluid
-                                    label='QTD de entregas realizadas'
-                                />
-
-                                <Form.Input
-                                    fluid
-                                    label='Valor por frete'
-                                />
-
-
-                            </Form.Group>
-
-                            <Form.Group widths='equal'>
-
-                                <Form.Input
-                                    fluid
-                                    label='Rua'
-                                    width={10}
-                                />
-
-                                <Form.Input
-                                    fluid
-                                    label='Número'
-                                />
-
-
-                            </Form.Group>
-
-                            <Form.Group>
-
-                                <Form.Input
-                                    fluid
-                                    label='Bairro'
-                                />
-
-                                <Form.Input
-                                    fluid
-                                    label='Cidade'
-                                />
-
-                                <Form.Input
-                                    fluid
-                                    label='CEP'
-                                />
-
-                            </Form.Group>
-
-                            <Form.Group>
-                                <Select placeholder='Selecione' options={countryOptions} />
-                            </Form.Group>
-                            
-
-
-
-                            <Form.Input
-                                fluid
-                                label='Complemento'
-                            />
-
-                            <Form.Input
-                                fluid
-                                label='Complemento'
-                            />
-
-
-
-
-
-
-                        </Form>
-
-                        <div style={{ marginTop: '4%' }}>
-
-                            <Button
-                                type="button"
-                                inverted
-                                circular
-                                icon
-                                labelPosition='left'
-                                color='orange'
-                            >
-                                <Icon name='reply' />
-                                Listar
-                            </Button>
-
-                            <Button
-                                inverted
-                                circular
-                                icon
-                                labelPosition='left'
-                                color='blue'
-                                floated='right'
-                            >
-                                <Icon name='save' />
-                                Salvar
-                            </Button>
-
-                        </div>
-
+                        <Button
+                            type="button"
+                            inverted
+                            circular
+                            icon
+                            labelPosition='left'
+                            color='orange'
+                        >
+                            <Icon name='reply' />
+                            Listar
+                        </Button>
+                        <Button
+                            inverted
+                            circular
+                            icon
+                            labelPosition='left'
+                            color='blue'
+                            floated='right'
+                        >
+                            <Icon name='save' />
+                            Salvar
+                        </Button>
                     </div>
-
-                </Container>
-            </div>
+                </Form>
+            </Container>
         </div>
 
     );
-
 }
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import { Container, Grid, Image } from 'semantic-ui-react';
 
-export default function Home () {
+export default function Home() {
 
-   return(
-       <div>
+    return (
+        <div>
 
-           <div style={{marginTop: '5%'}}>
-               <Container>
-                   <Grid columns={2} divided>
-                       <Grid.Row>
-                           <Grid.Column>
-                               <Image src='/logo-IFPE.png' size='large' />
-                           </Grid.Column>
-                           <Grid.Column>
-                              
-                               Bem vindo ao sistema <strong>OxeFood</strong> ! <br/>
-                               Este sistema foi desenvolvido na disciplina de Desenvolvimento para WEB III. <br/> <br/>
-                               Para acessar o código da <strong>API</strong> do sistema, acesse: <a href='https://github.com/JaideneMaria/oxefood-api-JaideneMaria.git' target='_blank'> https://github.com/JaideneMaria/oxefood-api-JaideneMaria.git </a> <br/> <br/>
-                               Para acessar o código do <strong>Módulo WEB</strong>, acesse: <a href='https://github.com/JaideneMaria/oxefood-web-jaidene.git' target='_blank'> https://github.com/JaideneMaria/oxefood-web-jaidene.git </a>
+            <div style={{ marginTop: '5%' }}>
+                <Container>
+                    <Grid columns={2} divided>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Image src='/logo-IFPE.png' size='large' />
+                            </Grid.Column>
+                            <Grid.Column>
 
-                           </Grid.Column>
-                       </Grid.Row>
-                   </Grid>
-               </Container>
-           </div>
-       </div>
-   )
+                                Bem vindo ao sistema <strong>OxeFood</strong> ! <br />
+                                Este sistema foi desenvolvido na disciplina de Desenvolvimento para WEB III. <br /> <br />
+                                Para acessar o código da <strong>API</strong> do sistema, acesse: <a href='https://github.com/JaideneMaria/oxefood-api-JaideneMaria.git' target='_blank'> https://github.com/JaideneMaria/oxefood-api-JaideneMaria.git </a> <br /> <br />
+                                Para acessar o código do <strong>Módulo WEB</strong>, acesse: <a href='https://github.com/JaideneMaria/oxefood-web-jaidene.git' target='_blank'> https://github.com/JaideneMaria/oxefood-web-jaidene.git </a>
+
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Container>
+            </div>
+        </div>
+    )
 }
